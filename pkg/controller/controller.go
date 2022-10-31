4431bc2a012b2f30e96da4ebd282b46385769759
suryaadi44 <suryawarior44@gmail.com>
2022-10-31 14:12:07 +0800

    [#5] feat: echo implementation

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,1 +1,13 @@
 package controller
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
+)
+
+func InitController(e *echo.Echo, _ *gorm.DB) {
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Recover())
+
+}
