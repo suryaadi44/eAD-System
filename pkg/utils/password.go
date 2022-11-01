c1c34931bc82e2135a6ef3d812ad8972d4ad6f94
suryaadi44 <suryawarior44@gmail.com>
2022-11-01 13:11:38 +0800

    [#2] Fix unused method receiver

--- pkg/utils/password.go
+++ pkg/utils/password.go
@@ -5,10 +5,10 @@ import "golang.org/x/crypto/bcrypt"
 type PasswordFunc struct {
 }
 
-func (p PasswordFunc) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
+func (PasswordFunc) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
 	return bcrypt.GenerateFromPassword(password, cost)
 }
 
-func (p PasswordFunc) CompareHashAndPassword(hashedPassword, password []byte) error {
+func (PasswordFunc) CompareHashAndPassword(hashedPassword, password []byte) error {
 	return bcrypt.CompareHashAndPassword(hashedPassword, password)
 }
