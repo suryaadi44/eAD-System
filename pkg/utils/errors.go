8befb35af6a947c4b03e637a455294b515d9bc1e
suryaadi44 <suryawarior44@gmail.com>
2022-11-02 13:56:25 +0800

    [#34] feat: auto add default user account

--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -4,25 +4,48 @@ import "errors"
 
 // Controller errors
 var (
-	ErrBadRequestBody       = errors.New("bad request body")
-	ErrInvalidCredentials   = errors.New("invalid email or password")
-	ErrInvalidTemplateID    = errors.New("invalid template id")
-	ErrDocumentAccessDenied = errors.New("document access denied")
+	// ErrBadRequest is used when the request body is not valid format
+	ErrBadRequestBody = errors.New("bad request body")
+
+	// ErrInvalidCredentials is used when the user's credentials are invalid
+	ErrInvalidCredentials = errors.New("invalid username or password")
+
+	// ErrInvalidTemplateID is used when the template id is invalid or not found
+	ErrInvalidTemplateID = errors.New("invalid template id")
+
+	// ErrDidntHavePermission is used when the user doesn't have permission to access or modify the resource
+	ErrDidntHavePermission = errors.New("you didn't have permission to do this action")
 )
 
 // Service errors
 var (
+	// ErrFieldNotMatch is used when the field in the request body is not match with the field in the template that saved in the database
 	ErrFieldNotMatch = errors.New("document fields doesn't match with template fields")
 )
 
 // Repository errors
 var (
-	ErrUsernameAlreadyExist  = errors.New("user with provided username already exist")
-	ErrNIKAlreadyExist       = errors.New("user with provided nik already exist")
-	ErrNIPAlreadyExist       = errors.New("user with provided nip already exist")
-	ErrUserNotFound          = errors.New("user not found")
-	ErrTemplateNotFound      = errors.New("template not found")
+	// ErrUsernameAlreadyExist is used when the username is already exist in the database
+	ErrUsernameAlreadyExist = errors.New("user with provided username already exist")
+
+	// ErrNIKAlreadyExist is used when the NIK is already exist in the database
+	ErrNIKAlreadyExist = errors.New("user with provided nik already exist")
+
+	// ErrNIPAlreadyExist is used when the NIP is already exist in the database
+	ErrNIPAlreadyExist = errors.New("user with provided nip already exist")
+
+	// ErrUserNotFound is used when the user is not found in the database
+	ErrUserNotFound = errors.New("user not found")
+
+	// ErrTemplateNotFound is used when the template is not found in the database
+	ErrTemplateNotFound = errors.New("template not found")
+
+	// ErrTemplateFieldNotFound is used when the template field is not found in the database
 	ErrTemplateFieldNotFound = errors.New("template field not found")
-	ErrDuplicateRegister     = errors.New("document with provided register already exist")
-	ErrDocumentNotFound      = errors.New("document not found")
+
+	// ErrDuplicateRegister is used when the document register is already exist in the database
+	ErrDuplicateRegister = errors.New("document with provided register already exist")
+
+	// ErrDocumentNotFound is used when the document is not found in the database
+	ErrDocumentNotFound = errors.New("document not found")
 )
