416854e405fe035b800f192572ce2adbf0a0858e
suryaadi44 <suryawarior44@gmail.com>
2022-11-02 20:11:27 +0800

    test: add test

---
+++ internal/user/repository/user_repository_impl_test.go
@@ -0,0 +1,133 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/suite"
+	"github.com/suryaadi44/eAD-System/pkg/entity"
+	"github.com/suryaadi44/eAD-System/pkg/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"regexp"
+	"testing"
+)
+
+type TestSuiteUserRepository struct {
+	suite.Suite
+	mock           sqlmock.Sqlmock
+	userRepository UserRepository
+}
+
+func (s *TestSuiteUserRepository) SetupTest() {
+	dbMock, mock, err := sqlmock.New()
+	s.NoError(err)
+	s.mock = mock
+
+	DB, _ := gorm.Open(mysql.New(mysql.Config{
+		Conn:                      dbMock,
+		SkipInitializeWithVersion: true,
+	}), &gorm.Config{})
+
+	s.userRepository = NewUserRepositoryImpl(DB)
+}
+
+func (s *TestSuiteUserRepository) TeardownTest() {
+	s.mock = nil
+	s.userRepository = nil
+}
+
+func (s *TestSuiteUserRepository) TestCreateUser() {
+	query := regexp.QuoteMeta("INSERT INTO `users` (`id`,`n_ip`,`nik`,`username`,`password`,`role`,`name`,`telp`,`sex`,`address`,`created_at`,`updated_at`,`deleted_at`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)")
+	for _, tc := range []struct {
+		Name        string
+		Err         error
+		ExpectedErr error
+	}{
+		{
+			Name:        "Success",
+			Err:         nil,
+			ExpectedErr: nil,
+		},
+		{
+			Name:        "Error duplicate username",
+			Err:         errors.New("Error 1062: Duplicate entry '' for key 'username'"),
+			ExpectedErr: utils.ErrUsernameAlreadyExist,
+		},
+		{
+			Name:        "Error duplicate nip",
+			Err:         errors.New("Error 1062: Duplicate entry '' for key 'n_ip'"),
+			ExpectedErr: utils.ErrNIPAlreadyExist,
+		},
+		{
+			Name:        "Error duplicate nik",
+			Err:         errors.New("Error 1062: Duplicate entry '' for key 'nik'"),
+			ExpectedErr: utils.ErrNIKAlreadyExist,
+		},
+		{
+			Name:        "Generic error",
+			Err:         errors.New("Generic error"),
+			ExpectedErr: errors.New("Generic error"),
+		},
+	} {
+		s.SetupTest()
+		s.Run(tc.Name, func() {
+			s.mock.ExpectBegin()
+			if tc.Err != nil {
+				s.mock.ExpectExec(query).WillReturnError(tc.Err)
+				s.mock.ExpectRollback()
+			} else {
+				s.mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+				s.mock.ExpectCommit()
+			}
+
+			err := s.userRepository.CreateUser(context.Background(), &entity.User{})
+
+			s.Equal(tc.ExpectedErr, err)
+		})
+		s.TeardownTest()
+	}
+}
+
+func (s *TestSuiteUserRepository) TestFindByUsername() {
+	query := regexp.QuoteMeta("SELECT `id`,`username`,`password`,`role` FROM `users` WHERE username = ? AND `users`.`deleted_at` IS NULL ORDER BY `users`.`id` LIMIT 1")
+	for _, tc := range []struct {
+		Name        string
+		Err         error
+		ExpectedErr error
+	}{
+		{
+			Name:        "Success",
+			Err:         nil,
+			ExpectedErr: nil,
+		},
+		{
+			Name:        "Error no record found",
+			Err:         gorm.ErrRecordNotFound,
+			ExpectedErr: utils.ErrUserNotFound,
+		},
+		{
+			Name:        "Generic error",
+			Err:         errors.New("Generic error"),
+			ExpectedErr: errors.New("Generic error"),
+		},
+	} {
+		s.SetupTest()
+		s.Run(tc.Name, func() {
+			if tc.Err != nil {
+				s.mock.ExpectQuery(query).WillReturnError(tc.Err)
+			} else {
+				s.mock.ExpectQuery(query).WillReturnRows(sqlmock.NewRows([]string{"id", "username", "password", "role"}).AddRow(1, "123", "123", 1))
+			}
+
+			_, err := s.userRepository.FindByUsername(context.Background(), "")
+
+			s.Equal(tc.ExpectedErr, err)
+		})
+		s.TeardownTest()
+	}
+}
+
+func TestUserRepository(t *testing.T) {
+	suite.Run(t, new(TestSuiteUserRepository))
+}
---
+++ internal/user/service/user_service_impl_test.go
@@ -0,0 +1,182 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/suryaadi44/eAD-System/pkg/utils"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"github.com/suryaadi44/eAD-System/internal/user/dto"
+	"github.com/suryaadi44/eAD-System/pkg/entity"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) CreateUser(ctx context.Context, user *entity.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (m *MockUserRepository) FindByUsername(ctx context.Context, username string) (*entity.User, error) {
+	args := m.Called(ctx, username)
+	return args.Get(0).(*entity.User), args.Error(1)
+}
+
+type MockPasswordHashFunction struct {
+	mock.Mock
+}
+
+func (m *MockPasswordHashFunction) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
+	args := m.Called(password, cost)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (m *MockPasswordHashFunction) CompareHashAndPassword(hashedPassword, password []byte) error {
+	args := m.Called(hashedPassword, password)
+	return args.Error(0)
+}
+
+type MockJWTService struct {
+	mock.Mock
+}
+
+func (m *MockJWTService) GenerateToken(user *entity.User) (string, error) {
+	args := m.Called(user)
+	return args.String(0), args.Error(1)
+}
+
+type TestSuiteUserService struct {
+	suite.Suite
+	mockUserRepository *MockUserRepository
+	mockPasswordHash   *MockPasswordHashFunction
+	mockJWTService     *MockJWTService
+	userService        UserService
+}
+
+func (t *TestSuiteUserService) SetupTest() {
+	t.mockUserRepository = new(MockUserRepository)
+	t.mockPasswordHash = new(MockPasswordHashFunction)
+	t.mockJWTService = new(MockJWTService)
+	t.userService = NewUserServiceImpl(t.mockUserRepository, t.mockPasswordHash, t.mockJWTService)
+}
+
+func (t *TestSuiteUserService) TearDownTest() {
+	t.mockUserRepository = nil
+	t.mockPasswordHash = nil
+	t.mockJWTService = nil
+	t.userService = nil
+}
+
+func (t *TestSuiteUserService) TestSignUpUser_Success() {
+	t.mockPasswordHash.On("GenerateFromPassword", []byte("password"), 10).Return([]byte("hashedPassword"), nil)
+	t.mockUserRepository.On("CreateUser", mock.Anything, mock.Anything).Return(nil)
+
+	err := t.userService.SignUpUser(context.Background(), &dto.UserSignUpRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.NoError(err)
+}
+
+func (t *TestSuiteUserService) TestSignUpUser_FailedHashing() {
+	t.mockPasswordHash.On("GenerateFromPassword", []byte("password"), 10).Return([]byte(""), errors.New("error"))
+
+	err := t.userService.SignUpUser(context.Background(), &dto.UserSignUpRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.Error(errors.New("error"), err)
+}
+
+func (t *TestSuiteUserService) TestSignUpUser_FailedCreateUser() {
+	t.mockPasswordHash.On("GenerateFromPassword", []byte("password"), 10).Return([]byte("hashedPassword"), nil)
+	t.mockUserRepository.On("CreateUser", mock.Anything, mock.Anything).Return(errors.New("error"))
+
+	err := t.userService.SignUpUser(context.Background(), &dto.UserSignUpRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.Error(errors.New("error"), err)
+}
+
+func (t *TestSuiteUserService) TestLoginUser_Success() {
+	t.mockUserRepository.On("FindByUsername", mock.Anything, "username").Return(&entity.User{
+		Username: "username",
+		Password: "hashedPassword",
+	}, nil)
+	t.mockPasswordHash.On("CompareHashAndPassword", []byte("hashedPassword"), []byte("password")).Return(nil)
+	t.mockJWTService.On("GenerateToken", mock.Anything).Return("token", nil)
+
+	resp, err := t.userService.LogInUser(context.Background(), &dto.UserLoginRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.NoError(err)
+	t.Equal("token", resp)
+}
+
+func (t *TestSuiteUserService) TestLoginUser_FailedFindByUsername() {
+	t.mockUserRepository.On("FindByUsername", mock.Anything, "username").Return(&entity.User{}, errors.New("error"))
+
+	_, err := t.userService.LogInUser(context.Background(), &dto.UserLoginRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.Error(err)
+}
+
+func (t *TestSuiteUserService) TestLoginUser_FailedUserNotFound() {
+	t.mockUserRepository.On("FindByUsername", mock.Anything, "username").Return(&entity.User{}, utils.ErrUserNotFound)
+
+	_, err := t.userService.LogInUser(context.Background(), &dto.UserLoginRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.Equal(utils.ErrInvalidCredentials, err)
+}
+
+func (t *TestSuiteUserService) TestLoginUser_FailedCompareHashAndPassword() {
+	t.mockUserRepository.On("FindByUsername", mock.Anything, "username").Return(&entity.User{
+		Username: "username",
+		Password: "hashedPassword",
+	}, nil)
+	t.mockPasswordHash.On("CompareHashAndPassword", []byte("hashedPassword"), []byte("password")).Return(errors.New("error"))
+
+	_, err := t.userService.LogInUser(context.Background(), &dto.UserLoginRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.Equal(utils.ErrInvalidCredentials, err)
+}
+
+func (t *TestSuiteUserService) TestLoginUser_FailedGenerateToken() {
+	t.mockUserRepository.On("FindByUsername", mock.Anything, "username").Return(&entity.User{
+		Username: "username",
+		Password: "hashedPassword",
+	}, nil)
+	t.mockPasswordHash.On("CompareHashAndPassword", []byte("hashedPassword"), []byte("password")).Return(nil)
+	t.mockJWTService.On("GenerateToken", mock.Anything).Return("", errors.New("error"))
+
+	_, err := t.userService.LogInUser(context.Background(), &dto.UserLoginRequest{
+		Username: "username",
+		Password: "password",
+	})
+
+	t.Error(err)
+}
+
+func TestUserService(t *testing.T) {
+	suite.Run(t, new(TestSuiteUserService))
+}
